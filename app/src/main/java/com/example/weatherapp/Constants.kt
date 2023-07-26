14ee13cc64820d09e8a1cf9df5980b0b778953af
Jakub <jakub.kucaba@gmail.com>
2023-07-26 15:53:06 +0200

    Creating the API Call with Retrofit

--- app/src/main/java/com/example/weatherapp/Constants.kt
+++ app/src/main/java/com/example/weatherapp/Constants.kt
@@ -5,7 +5,9 @@ import android.net.NetworkCapabilities
 import android.os.Build
 
 object Constants {
-
+    const val APP_ID: String = R.string.APP_ID.toString()
+    const val BASE_URL: String = "https://api.openweathermap.org/data/"
+    const val METRIC_UNIT: String = "metric"
     fun isNetworkAvailable(context: Context): Boolean {
         val connectivityManager =
             context.getSystemService(
